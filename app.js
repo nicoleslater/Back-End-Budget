580886d071d2da3543ecdf6bb7b6b3e33079e30d
Nicole Slater <nicoleslater@pursuit.org>
2023-10-17 00:56:43 -0400

    Updating, Server is running

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+module.exports = app;
