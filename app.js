737ae5077fd5542250e7df5f9ccc47f9154ee4be
Nicole Slater <nicoleslater@pursuit.org>
2023-10-17 01:20:33 -0400

    Adding transactions to URL

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const transactions = require("./models/transactions.json");
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
 });
+
+app.get('/transactions', (req, res) => {
+    res.json(transactions);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
