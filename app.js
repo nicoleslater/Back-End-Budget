c7c9b35fc2c4af95578d3884645a6f09b93bc8b2
Nicole Slater <nicoleslater@pursuit.org>
2023-10-17 01:34:48 -0400

    Updating app.get in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
+
 const transactions = require("./models/transactions.json");
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -9,6 +12,28 @@ app.get('/', (req, res) => {
 
 app.get('/transactions', (req, res) => {
     res.json(transactions);
+});
+
+app.get('/transactions/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const transaction = transactions.find(item => item.id === id);
+
+    if(transaction){
+        res.json(transaction);
+    } else {
+        res.status(404).json({message: 'Transaction not found'});
+    }
+
+});
+
+app.post('/transactions', (req, res) => {
+    const newTransaction = req.body;
+    newTransaction.id = transactions.length > 0 ? Math.max(...transactions.map(t => t.id)) + 1 : 1;
+    transactions.push(newTransaction);
+
+    fs.writeFileSync(transactionsFilePath, JSON.stringify(transactions, null, 2));
+    
+    res.json(newTransaction);
 })
 
 app.listen(PORT, () => {
