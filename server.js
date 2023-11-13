b0ae7db3223491da94c495576d31076a830b748d
Nicole Slater <nicoleslater@pursuit.org>
2023-11-12 22:28:39 -0500

    Debugging server, adding queries and validations

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 require("dotenv").config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6565;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
