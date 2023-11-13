16bc0bc678d502b58865582bda3976189fe37690
Nicole Slater <nicoleslater@pursuit.org>
2023-11-12 23:51:12 -0500

    Correcting errors in app.js and transactionController

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 
-const transactions = require("./controllers/transactionController");
+const transactionsController = require("./controllers/transactionController");
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(cors());
 
 app.use(express.json());
 
-app.use("/transactions", transactions);
+app.use("/transactions", transactionsController);
 
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     
   res.send('Welcome to Budget App!');
 });
 
-app.get("/404", (req, res) => {
-  res.send("I'm sorry that page can not be found, please try again!");
-})
+app.get("*", (req, res) => {
+ res.status(404).json({success: false, data: { error: "Page is not found"}});
+});
+
+module.exports = app; 
 
 
 
