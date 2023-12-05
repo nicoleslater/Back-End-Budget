3df250c937cfef8764bc8970aede3283865f3c2e
Nicole Slater <nicoleslater@pursuit.org>
2023-12-05 16:21:22 -0500

    Updating Server and App

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 require("dotenv").config()
 
-const PORT = process.env.PORT || 6565;
+const PORT = process.env.PORT || 6567;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
