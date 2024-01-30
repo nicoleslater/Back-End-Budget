047cda47b057990546ad74dc6e9a15f9464c5cda
Nicole Slater <nicoleslater@pursuit.org>
2024-01-30 13:43:42 -0500

    Updated checkTransactions

--- back-end/validations/checkTransactions.js
+++ back-end/validations/checkTransactions.js
@@ -11,7 +11,7 @@ const checkBoolean = (req, res, next) => {
     if (req.body.priority === true || req.body.priority === false){
         next()
     } else {
-        res.status(400).json({error: "is_priority must be a boolean value"})
+        res.status(400).json({error: "priority must be a boolean value"})
     }
 }
 
